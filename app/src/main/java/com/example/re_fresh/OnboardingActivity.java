d57f5a170f7241daceed69b38f4f56e3bf1ebf12
İlayda Karacuka <ilaydakaracuka1@gmail.com>
2025-04-17 01:53:02 +0300

    SplashActivity ve Onboarding ekranları yapıldı.

--- app/src/main/java/com/example/re_fresh/OnboardingActivity.java
+++ app/src/main/java/com/example/re_fresh/OnboardingActivity.java
@@ -1,24 +1,64 @@
 package com.example.re_fresh;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class OnboardingActivity extends AppCompatActivity {
+
+    private int currentPage = 0;
+
+    private ImageView onboardingImage, pagination;
+    private TextView titleText, descText;
+    private Button skipButton, nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_onboarding);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        onboardingImage = findViewById(R.id.onboardingImage);
+        pagination = findViewById(R.id.paginationImage);
+        titleText = findViewById(R.id.titleText);
+        descText = findViewById(R.id.descriptionText);
+        skipButton = findViewById(R.id.skipButton);
+        nextButton = findViewById(R.id.nextButton);
+
+        skipButton.setOnClickListener(v -> {
+            Intent intent = new Intent(OnboardingActivity.this, WelcomeActivity.class);
+            startActivity(intent);
+            finish();
         });
+
+        nextButton.setOnClickListener(v -> {
+            currentPage++;
+            updateContent();
+        });
+    }
+
+    private void updateContent() {
+        switch (currentPage) {
+            case 1:
+                onboardingImage.setImageResource(R.drawable.onboarding_2);
+                pagination.setImageResource(R.drawable.pagination_2);
+                titleText.setText("Zamanında\nbildirim al!");
+                descText.setText("Tarihi yaklaşan ürünler için \nhatırlatma al, israfı önle.");
+                break;
+            case 2:
+                onboardingImage.setImageResource(R.drawable.onboarding_3);
+                pagination.setImageResource(R.drawable.pagination_3);
+                titleText.setText("Daha Bilinçli Tüketim!");
+                descText.setText("Tasarruf et, doğaya katkı sağla.");
+                nextButton.setText("Başla");
+                break;
+            case 3:
+                startActivity(new Intent(OnboardingActivity.this, WelcomeActivity.class));
+                finish();
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/re_fresh/SplashActivity.java
+++ app/src/main/java/com/example/re_fresh/SplashActivity.java
@@ -1,24 +1,28 @@
 package com.example.re_fresh;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Handler kullanarak 3 saniye bekleyip yeni aktiviteye geçiş yap
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, OnboardingActivity.class);
+            startActivity(intent);
+            finish(); // Splash ekranını kapat
+        },1800);
     }
 }
